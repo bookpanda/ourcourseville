c4ed53446916cc1028f41e4136fe38a10c35825c
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-24 19:25:18 +0700

    token

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,23 @@ import { chromium } from "playwright";
 (async () => {
   let browser: Browser | null = null;
   try {
-    // Launch browser
+    const refreshToken = process.env.REFRESH_TOKEN ?? ""; // Replace with your actual refresh token
+    console.log("Refresh Token:", refreshToken);
+
     browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page: Page = await context.newPage();
 
-    // Navigate to the website
     await page.goto("https://alpha.mycourseville.com");
 
     // Inject refresh token into localStorage
-    const refreshToken = process.env.REFRESH_TOKEN ?? ""; // Replace with your actual refresh token
-    console.log("Refresh Token:", refreshToken);
 
     await page.evaluate((token) => {
       localStorage.setItem("refresh_token", token);
     }, refreshToken);
 
     // Reload or go to the course page to ensure you're authenticated
-    await page.goto("https://alpha.mycourseville.com/course/0000-KMh/");
+    // await page.goto("https://alpha.mycourseville.com/course/0000-KMh/");
 
     // Scrape the data from the course page
     const scrapedData = await page.evaluate(() => {
@@ -40,8 +39,8 @@ import { chromium } from "playwright";
   } catch (error) {
     console.error("Error during scraping:", error);
   } finally {
-    if (browser) {
-      await browser.close();
-    }
+    // if (browser) {
+    //   await browser.close();
+    // }
   }
 })();
