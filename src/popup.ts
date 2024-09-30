619b4d64f13a05a814b8518cbabd8375809c5048
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-30 22:45:35 +0700

    text/choice ans

--- src/popup.ts
+++ src/popup.ts
@@ -26,11 +26,52 @@ function scrapePage() {
   const innerMain = mainElements[1];
   const form = innerMain.querySelectorAll("form")[0];
 
-  const questions = Array.from(form.children).filter(
+  const problems = Array.from(form.children).filter(
     (child) => child.tagName === "DIV"
   );
 
-  chrome.runtime.sendMessage({ title: pageTitle, questions: questions });
+  const qnas = problems.map((p) => {
+    const questionDiv = p.children[0].children[0];
+    const questionP = questionDiv.getElementsByTagName("p");
+    const textArray = Array.from(questionP).map((para) =>
+      para.textContent?.trim()
+    );
+    const question = textArray.join(" ");
 
-  return { title: pageTitle, questions: questions }; // Also return it for popup use
+    const answerDiv = p.children[0].children[1];
+    const textAnswer = Array.from(answerDiv.childNodes)
+      .reduce((text, node) => {
+        if (node.nodeType === Node.TEXT_NODE) {
+          return text + node?.textContent?.trim();
+        }
+        return text;
+      }, "")
+      .trim();
+    if (textAnswer) {
+      return { question, answer: textAnswer };
+    }
+
+    const checkedButton = answerDiv.querySelector(
+      'button[data-state="checked"]'
+    );
+    if (!checkedButton) {
+      console.log("No checked button found in the specified <div>.");
+      return { question, answer: "No answer found" };
+    }
+
+    const label = document.querySelector(`label[for="${checkedButton.id}"]`);
+    const choiceAnswer = label?.textContent?.trim() ?? "No answer found";
+
+    return {
+      question,
+      answer: choiceAnswer,
+    };
+  });
+
+  chrome.runtime.sendMessage({
+    title: pageTitle,
+    problems: qnas,
+  });
+
+  return { title: pageTitle, problems: qnas }; // Also return it for popup use
 }
