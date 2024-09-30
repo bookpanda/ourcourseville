23f35e29ad1efa1b3a9d365609e111568f3cef0b
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-30 22:13:24 +0700

    2

--- build/background.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log("Received scraped data:", message);
-    // You can do something with the data here, like saving it or processing it
-    sendResponse({ status: "Data received" });
-});
