8f53d229b5861980ec353f5454bf5c8554d91857
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-01 15:49:07 +0700

    scrape script

--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,4 @@
-import { ScrapeMessage } from "./types";
+import { scrapePage } from "./scripts/scrape";
 
 const btn = document.getElementById("scrapeButton");
 
@@ -10,69 +10,9 @@ btn?.addEventListener("click", () => {
         func: scrapePage,
       },
       (results) => {
-        console.log("results:", results);
         const message = results[0].result;
-        console.log("message:", message);
-        // chrome.runtime.sendMessage(message);
+        chrome.runtime.sendMessage(message);
       }
     );
   });
 });
-
-const scrapePage = (): ScrapeMessage => {
-  const pageTitle = document.title;
-  const mainElements = document.querySelectorAll("main");
-  const innerMain = mainElements[1];
-  const form = innerMain.querySelectorAll("form")[0];
-
-  const problems = Array.from(form.children).filter(
-    (child) => child.tagName === "DIV"
-  );
-
-  const qnas = problems.map((p) => {
-    const questionDiv = p.children[0].children[0];
-    const questionP = questionDiv.getElementsByTagName("p");
-    const textArray = Array.from(questionP).map((para) =>
-      para.textContent?.trim()
-    );
-    const question = textArray.join(" ");
-
-    const answerDiv = p.children[0].children[1];
-    const textAnswer = Array.from(answerDiv.childNodes)
-      .reduce((text, node) => {
-        if (node.nodeType === Node.TEXT_NODE) {
-          return text + node?.textContent?.trim();
-        }
-        return text;
-      }, "")
-      .trim();
-    if (textAnswer) return { question, answer: textAnswer };
-
-    const checkedButton = answerDiv.querySelector(
-      'button[data-state="checked"]'
-    );
-    const label = document.querySelector(`label[for="${checkedButton?.id}"]`);
-    const choiceAnswer = label?.textContent?.trim();
-    if (choiceAnswer)
-      return {
-        question,
-        answer: choiceAnswer,
-      };
-
-    const fileAnchor = answerDiv.querySelector("a");
-    const fileUrl = fileAnchor?.href;
-    if (fileUrl) return { question, answer: fileUrl };
-
-    return { question, answer: "No answer found" };
-  });
-
-  const response: ScrapeMessage = {
-    type: "scrape",
-    title: pageTitle,
-    problems: qnas,
-  };
-
-  // chrome.runtime.sendMessage(response);
-
-  return response;
-};
---
+++ src/scripts/scrape.ts
@@ -0,0 +1,59 @@
+import { ScrapeMessage } from "../types";
+
+export const scrapePage = (): ScrapeMessage => {
+  const pageTitle = document.title;
+  const mainElements = document.querySelectorAll("main");
+  const innerMain = mainElements[1];
+  const form = innerMain.querySelectorAll("form")[0];
+
+  const problems = Array.from(form.children).filter(
+    (child) => child.tagName === "DIV"
+  );
+
+  const qnas = problems.map((p) => {
+    const questionDiv = p.children[0].children[0];
+    const questionP = questionDiv.getElementsByTagName("p");
+    const textArray = Array.from(questionP).map((para) =>
+      para.textContent?.trim()
+    );
+    const question = textArray.join(" ");
+
+    const answerDiv = p.children[0].children[1];
+    const textAnswer = Array.from(answerDiv.childNodes)
+      .reduce((text, node) => {
+        if (node.nodeType === Node.TEXT_NODE) {
+          return text + node?.textContent?.trim();
+        }
+        return text;
+      }, "")
+      .trim();
+    if (textAnswer) return { question, answer: textAnswer };
+
+    const checkedButton = answerDiv.querySelector(
+      'button[data-state="checked"]'
+    );
+    const label = document.querySelector(`label[for="${checkedButton?.id}"]`);
+    const choiceAnswer = label?.textContent?.trim();
+    if (choiceAnswer)
+      return {
+        question,
+        answer: choiceAnswer,
+      };
+
+    const fileAnchor = answerDiv.querySelector("a");
+    const fileUrl = fileAnchor?.href;
+    if (fileUrl) return { question, answer: fileUrl };
+
+    return { question, answer: "No answer found" };
+  });
+
+  const response: ScrapeMessage = {
+    type: "scrape",
+    title: pageTitle,
+    problems: qnas,
+  };
+
+  // chrome.runtime.sendMessage(response);
+
+  return response;
+};
