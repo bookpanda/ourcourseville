25e6d780a9b761f311c14e0eb16a37b65c1c1e68
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-02 12:43:40 +0700

    model record firestore prop

--- backend/Models/Record.cs
+++ backend/Models/Record.cs
@@ -1,21 +1,33 @@
-using System.Collections.Generic;
 using Google.Cloud.Firestore;
 
 namespace backend.Models;
 
+[FirestoreData]
 public class Record
 {
+    [FirestoreProperty]
+    public string? ID { get; set; }
+    [FirestoreProperty]
     public required string CourseCode { get; set; }
+    [FirestoreProperty]
     public required string CourseID { get; set; }
+    [FirestoreProperty]
     public required string Course { get; set; }
+    [FirestoreProperty]
     public required string AssignmentID { get; set; }
+    [FirestoreProperty]
     public required string Assignment { get; set; }
+    [FirestoreProperty]
     public required List<Problem> Problems { get; set; }
+    [FirestoreProperty]
     public required Timestamp CreatedAt { get; set; }
 }
 
+[FirestoreData]
 public class Problem
 {
+    [FirestoreProperty]
     public required string Question { get; set; }
+    [FirestoreProperty]
     public required string Answer { get; set; }
 }
