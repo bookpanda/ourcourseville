a0d5179f1079de1ce5186628a75ec63c4c2b1e8b
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-03 12:22:01 +0700

    conf

--- backend/Config/Config.cs
+++ backend/Config/Config.cs
@@ -1,8 +1,10 @@
 namespace backend.Config;
 
-public class RecordConfig
+public class FirestoreConfig
 {
-    public const string Record = "Record";
+    public const string Firestore = "Firestore";
     public required string DB { get; set; }
-    public required string Collection { get; set; }
+    public required string Faculties { get; set; }
+    public required string Courses { get; set; }
+    public required string Records { get; set; }
 }
