79388c48d744b426d20f14c55220c167ad334328
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-06 14:33:23 +0700

    apikey conf

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
-import { API_URL } from "../config/config";
+import { API_KEY, API_URL } from "../config/config";
 
 export const apiClient = axios.create({
   baseURL: `${API_URL}/api/v1`,
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
   timeout: 10000,
 });
--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,4 +1,5 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_KEY = process.env.API_KEY;
 export const FACULTY_TTL = parseInt(
   process.env.NEXT_PUBLIC_FACULTY_TTL ?? "",
   10
