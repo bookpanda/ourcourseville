9dcba87bc609c91cf3c0efa0e9bb1182c4fc9f8c
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-06 22:07:08 +0700

    favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon/favicon-48x48.png"
+        sizes="48x48"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <body
         className={`${IBMPlex.className} max-w-screen bg-default flex min-h-screen flex-col`}
       >
