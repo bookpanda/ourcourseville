62fc4ba505aba6010c9246488b6a7ff94da948e6
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-06 14:24:11 +0700

    copy record

--- frontend/src/components/Card/RecordCard.tsx
+++ frontend/src/components/Card/RecordCard.tsx
@@ -1,3 +1,4 @@
+import { toast } from "@/hooks/use-toast";
 import { Record } from "@/src/types";
 import { formatTime } from "@/src/utils/formatTime";
 import { FC } from "react";
@@ -12,8 +13,17 @@ export const RecordCard: FC<RecordCardProps> = ({ href, record }) => {
   const { id, createdAt } = record;
   const formattedDate = formatTime(createdAt);
 
+  const handleClick = () => {
+    navigator.clipboard.writeText(record.id);
+
+    toast({
+      title: "Copied",
+      description: record.id,
+    });
+  };
+
   return (
-    <Card href={href}>
+    <Card href={href} onClick={handleClick}>
       <div className="grid items-center gap-2 lg:grid-cols-[auto,188px,188px]">
         <div className="flex gap-2">
           <h5 className="h6 lg:h5 font-semibold text-primary-default">{id}</h5>
--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -3,12 +3,16 @@ import { FC, PropsWithChildren } from "react";
 
 interface CardProps extends PropsWithChildren {
   href: string;
+  onClick?: () => void;
 }
 
-export const Card: FC<CardProps> = ({ href, children }) => {
+export const Card: FC<CardProps> = ({ onClick, href, children }) => {
   return (
     <Link href={href} className="w-full">
-      <div className="border-disable m-0 flex w-full min-w-[188px] cursor-pointer flex-col gap-2 rounded-xl border bg-white p-4 shadow-sm transition-all duration-200 ease-in-out hover:translate-y-[-4px] hover:shadow-md">
+      <div
+        onClick={onClick}
+        className="border-disable m-0 flex w-full min-w-[188px] cursor-pointer flex-col gap-2 rounded-xl border bg-white p-4 shadow-sm transition-all duration-200 ease-in-out hover:translate-y-[-4px] hover:shadow-md"
+      >
         {children}
       </div>
     </Link>
