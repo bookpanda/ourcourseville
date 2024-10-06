e50b447c7eff50a8cb3ea55d1941ba6c54057dab
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-06 22:29:59 +0700

    markup

--- extension/src/popup.ts
+++ extension/src/popup.ts
@@ -1,8 +1,9 @@
+import { load } from "./scripts/load";
 import { scrape } from "./scripts/scrape";
 
-const btn = document.getElementById("scrapeButton");
+const scrapeBtn = document.getElementById("scrapeButton");
 
-btn?.addEventListener("click", () => {
+scrapeBtn?.addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentTab = tabs[0];
 
@@ -11,6 +12,27 @@ btn?.addEventListener("click", () => {
         target: { tabId: currentTab.id ?? 0 },
         func: scrape,
         args: [currentTab.url ?? ""],
+      },
+      (results) => {
+        const message = results[0].result;
+        chrome.runtime.sendMessage(message);
+      }
+    );
+  });
+});
+
+const loadBtn = document.getElementById("loadButton");
+const loadInput = document.getElementById("solutionInput");
+
+loadBtn?.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: currentTab.id ?? 0 },
+        func: load,
+        args: [loadInput?.nodeValue ?? ""],
       },
       (results) => {
         const message = results[0].result;
