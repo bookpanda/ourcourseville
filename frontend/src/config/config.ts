3c74584f32570323ce1887add2f87dfdf09998ed
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-07 11:55:22 +0700

    API_URL

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.API_URL;
 export const API_KEY = process.env.API_KEY;
 export const FACULTY_TTL = parseInt(
   process.env.NEXT_PUBLIC_FACULTY_TTL ?? "",
