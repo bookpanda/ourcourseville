c84804434dcc87c420ba6e652b47e913a9d52565
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-07 10:45:00 +0700

    getFacultyBycode

--- frontend/src/api/faculty.ts
+++ frontend/src/api/faculty.ts
@@ -1,6 +1,10 @@
 import { AxiosResponse } from "axios";
 import { apiClient } from "./axios";
-import { FacultyDTO, parseFacultyDTOList } from "./dto/faculty.dto";
+import {
+  FacultyDTO,
+  parseFacultyDTO,
+  parseFacultyDTOList,
+} from "./dto/faculty.dto";
 
 export const getAllFaculty = async () => {
   try {
@@ -11,5 +15,18 @@ export const getAllFaculty = async () => {
   } catch (error) {
     console.error("Failed to get all faculty", error);
     return Error("Failed to get all faculty");
+  }
+};
+
+export const getFacultyByCode = async (code: string) => {
+  try {
+    const res: AxiosResponse<FacultyDTO> = await apiClient.get(
+      `/faculty/${code}`
+    );
+
+    return parseFacultyDTO(res.data);
+  } catch (error) {
+    console.error("Failed to get faculty by code", error);
+    return Error("Failed to get faculty by code");
   }
 };
