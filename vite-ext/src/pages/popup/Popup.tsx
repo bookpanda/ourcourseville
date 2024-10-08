bca9f2382b32284898da29fb6a6046e5864914da
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-08 22:47:29 +0700

    d

--- vite-ext/src/pages/popup/Popup.tsx
+++ vite-ext/src/pages/popup/Popup.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import logo from "@assets/img/logo.svg";
 
 export default function Popup(): JSX.Element {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
+    <div className="absolute bottom-0 left-0 right-0 top-0 h-full bg-gray-800 p-3 text-center">
       <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
+        <img
+          src={logo}
+          className="pointer-events-none h-36 animate-spin-slow"
+          alt="logo"
+        />
         <p>
           Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
         </p>
@@ -15,7 +18,7 @@ export default function Popup(): JSX.Element {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React
         </a>
         <p>Popup styled with TailwindCSS!</p>
       </header>
