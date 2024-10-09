cf7f1d8e29d183aeccf670a41bb02dba347b1294
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-09 16:53:16 +0700

    useGetRecentCourses

---
+++ frontend/src/hooks/useGetRecentCourses.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { cache } from "../cache/localStoage";
+import { selectRecentCourses, setRecentCourses } from "../store/courseSlice";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { Course } from "../types";
+
+export const useGetRecentCourses = () => {
+  const dispatch = useAppDispatch();
+  const recentCourses = useAppSelector(selectRecentCourses);
+
+  useEffect(() => {
+    const res = cache.getItem<Course[]>("recentCourses");
+    if (res) {
+      dispatch(setRecentCourses(res));
+      return;
+    }
+  }, []);
+
+  return { recentCourses };
+};
--- frontend/src/store/courseSlice.ts
+++ frontend/src/store/courseSlice.ts
@@ -5,11 +5,13 @@ import { RootState } from "./store";
 interface CourseState {
   courses: Course[];
   currentCourse: Course | null;
+  recentCourses: Course[];
 }
 
 const initialState: CourseState = {
   courses: [],
   currentCourse: null,
+  recentCourses: [],
 };
 
 export const courseSlice = createSlice({
@@ -22,12 +24,23 @@ export const courseSlice = createSlice({
     setCurrentCourse: (state, action: PayloadAction<Course>) => {
       state.currentCourse = action.payload;
     },
+    setRecentCourses: (state, action: PayloadAction<Course[]>) => {
+      state.recentCourses = action.payload;
+    },
+    pushRecentCourses: (state, action: PayloadAction<Course>) => {},
   },
 });
 
-export const { setCourses, setCurrentCourse } = courseSlice.actions;
+export const {
+  setCourses,
+  setCurrentCourse,
+  setRecentCourses,
+  pushRecentCourses,
+} = courseSlice.actions;
 export const selectCourses = (state: RootState) => state.course.courses;
 export const selectCurrentCourse = (state: RootState) =>
   state.course.currentCourse;
+export const selectRecentCourses = (state: RootState) =>
+  state.course.recentCourses;
 
 export default courseSlice.reducer;
