2ab5f3e081197976dc713c23874520a6bc07db25
Idhibhat Pankam <marginpankam@gmail.com>
2024-10-09 22:42:05 +0700

    NEXT_PUBLIC_EXTENSION_URL

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,6 +1,6 @@
 export const API_URL = process.env.API_URL;
 export const API_KEY = process.env.API_KEY;
-export const EXTENSION_URL = process.env.EXTENSION_URL ?? "";
+export const EXTENSION_URL = process.env.NEXT_PUBLIC_EXTENSION_URL ?? "";
 
 export const RECENT_COURSES_TTL = parseInt(
   process.env.NEXT_PUBLIC_RECENT_COURSES_TTL ?? "",
